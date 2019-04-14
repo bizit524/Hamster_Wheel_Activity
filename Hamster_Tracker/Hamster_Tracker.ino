43a76ab8e39a6dbad1b253e1c296dde0a1e3266e
bizit524 <bizit524@gmail.com>
2019-04-14 20:57:36 +0100

    base example

--- Hamster_Tracker/Hamster_Tracker.ino
+++ Hamster_Tracker/Hamster_Tracker.ino
@@ -1,9 +1,135 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiUdp.h>
+#include <WiFiManager.h>
+// Update these with values suitable for your network.
+
+const char* mqtt_server = "ha.lan";
+const char* topic = "Tracker";    // rhis is the [root topic]
+
+long timeBetweenMessages = 1000 * 20 * 1;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+int value = 0;
+
+int status = WL_IDLE_STATUS;     // the starting Wifi radio's status
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+}
+
+
+String macToStr(const uint8_t* mac)
+{
+  String result;
+  for (int i = 0; i < 6; ++i) {
+    result += String(mac[i], 16);
+    if (i < 5)
+      result += ':';
+  }
+  return result;
+}
+
+String composeClientID() {
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+  String clientId;
+  clientId += "esp-";
+  clientId += macToStr(mac);
+  return clientId;
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+
+    String clientId = "Trackers" ;
+
+
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+//      client.publish(topic, (clientId);
+      // ... and resubscribe
+      // topic + clientID + in
+      String subscription;
+      subscription += topic;
+      subscription += "/";
+      subscription += clientId ;
+      subscription += "/in";
+      client.subscribe(subscription.c_str() );
+      Serial.print("subscribed to : ");
+      Serial.println(subscription);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.print(" wifi=");
+      Serial.print(WiFi.status());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  
+  Serial.begin(115200);
+  // We start by connecting to a WiFi network
+  WiFiManager MyWifiManager;
+  MyWifiManager.autoConnect("Trackers");
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // confirm still connected to mqtt server
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 
+  long now = millis();
+  if (now - lastMsg > timeBetweenMessages ) {
+    lastMsg = now;
+    ++value;
+    String payload = "{\"micros\":";
+    payload += micros();
+    payload += ",\"counter\":";
+    payload += value;
+    payload += ",\"client\":";
+//    payload += clientId;
+    payload += "}";
+    String pubTopic;
+     pubTopic += topic ;
+ //    pubTopic += "/";
+//     pubTopic += clientId;
+     pubTopic += "/out";
+    Serial.print("Publish topic: ");
+    Serial.println(pubTopic);
+    Serial.print("Publish message: ");
+    Serial.println(payload);
+    client.publish( (char*) pubTopic.c_str() , (char*) payload.c_str(), true );
+  }
 }
