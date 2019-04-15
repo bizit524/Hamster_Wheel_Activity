7e65ecde51ab64894ec27dec38eacdb307fe5880
bizit524 <bizit524@gmail.com>
2019-04-15 20:04:06 +0100

    tracking wheel diameter /rotations and distance

--- Hamster_Tracker/Hamster_Tracker.ino
+++ Hamster_Tracker/Hamster_Tracker.ino
@@ -4,56 +4,34 @@
 #include <WiFiUdp.h>
 #include <WiFiManager.h>
 // Update these with values suitable for your network.
-
 const char* mqtt_server = "ha.lan";
-const char* topic = "Tracker";    // rhis is the [root topic]
+const char* topic = "Tracker";    // this is the [root topic]
+const char* starttopic = "Online"; 
+long timeBetweenMessages = 5000;
 
-long timeBetweenMessages = 1000 * 20 * 1;
+int rotations =0;
+int wheelspeed = 0;
+//diameter of inside of wheel
+int wheeldiamter = 293;
+int distance = 0;
+int period = 10000;
+unsigned long time_now=0;
+int startup = 0;
+
+
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
 int value = 0;
-
-int status = WL_IDLE_STATUS;     // the starting Wifi radio's status
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-}
-
-
-String macToStr(const uint8_t* mac)
-{
-  String result;
-  for (int i = 0; i < 6; ++i) {
-    result += String(mac[i], 16);
-    if (i < 5)
-      result += ':';
-  }
-  return result;
-}
-
+//define reed switch
+int ReedState = 0;
+int LastReedState = 0;
+const int  ReedPin = D6;  
 String composeClientID() {
   uint8_t mac[6];
-  WiFi.macAddress(mac);
   String clientId;
   clientId += "esp-";
-  clientId += macToStr(mac);
   return clientId;
 }
 
@@ -67,20 +45,7 @@ void reconnect() {
 
     // Attempt to connect
     if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-//      client.publish(topic, (clientId);
-      // ... and resubscribe
-      // topic + clientID + in
-      String subscription;
-      subscription += topic;
-      subscription += "/";
-      subscription += clientId ;
-      subscription += "/in";
-      client.subscribe(subscription.c_str() );
-      Serial.print("subscribed to : ");
-      Serial.println(subscription);
-    } else {
+       } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.print(" wifi=");
@@ -93,14 +58,15 @@ void reconnect() {
 }
 
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
-  
+
+
   Serial.begin(115200);
   // We start by connecting to a WiFi network
   WiFiManager MyWifiManager;
   MyWifiManager.autoConnect("Trackers");
   client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+    //setup reed switch need to pull down 
+    pinMode(ReedPin, INPUT_PULLUP);
 }
 
 void loop() {
@@ -108,21 +74,59 @@ void loop() {
   if (!client.connected()) {
     reconnect();
   }
-  client.loop();
-
-  long now = millis();
-  if (now - lastMsg > timeBetweenMessages ) {
-    lastMsg = now;
-    ++value;
-    String payload;;
-    payload += value;
-
+  if ( startup == 0)
+  {// this is to make sure it connects by publishing a topic else the reconnect will sit 
+    startup ++;
+    String payload = "ESP";
     String pubTopic;
-     pubTopic += topic ;
-    Serial.print("Publish topic: ");
-    Serial.println(pubTopic);
-    Serial.print("Publish message: ");
-    Serial.println(payload);
+    
+    pubTopic += starttopic;
     client.publish( (char*) pubTopic.c_str() , (char*) payload.c_str(), true );
+    Serial.println(pubTopic);
+    Serial.println(payload);
   }
+  //setup pin 
+    ReedState = digitalRead(ReedPin);
+    long now = millis();
+
+    //check if wheel is running if it is increment rotations
+
+       if (ReedState != LastReedState )
+       {
+          if (ReedState == LOW)
+          { Serial.println("Wheel Cycle");
+            rotations ++;
+            //reset counter
+            lastMsg = now;
+           }
+           else {}
+         delay(50);
+        
+        }
+        LastReedState = ReedState;
+      //if there has been movement and it is after 10 seconds of no activity then publish results 
+        if (now - lastMsg > timeBetweenMessages ) {
+          distance = rotations * (wheeldiamter*3.14);
+          
+          lastMsg = now;
+          ++value;
+          String payload = "{\"rotations\":";
+          payload += rotations;
+          payload += ",\"speed\":";
+          payload += wheelspeed;
+          payload += ",\"distance\":";
+          payload += distance;
+          payload += "}";
+          String pubTopic;
+           pubTopic += topic ;
+          Serial.print("Publish topic: ");
+          Serial.println(pubTopic);
+          Serial.print("Publish message: ");
+          Serial.println(payload);
+          client.publish( (char*) pubTopic.c_str() , (char*) payload.c_str(), true );
+          distance = 0;
+          rotations = 0;
+          wheelspeed = 0;   
+      }
+
 }
